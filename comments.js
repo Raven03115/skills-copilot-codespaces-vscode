bdd77f2bf7d25635f39cfdb03249895bad22504b
Raven <70535765+Raven03115@users.noreply.github.com>
2023-08-04 16:24:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,88 +1,67 @@
 // Create  web server
 
-// Import modules
-const express = require('express');
-const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+// 1. Import module
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var shortid = require('shortid');
+var db = require('./db');
 
-// Import models
-const Comment = require('../models/comment.js');
+// 2. Create app
+var app = express();
 
-// Import middleware
-const auth = require('../middleware/auth.js');
+// 3. Set up app
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-// @route   GET api/comments
-// @desc    Get all comments
-// @access  Public
-router.get('/', async (req, res) => {
-  try {
-    const comments = await Comment.find().sort({ date: -1 });
-    res.json(comments);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Server error (GET api/comments)');
-  }
+// 4. Use middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// 5. Create router
+app.get('/', function(req, res){
+	res.render('index', {
+		name: 'AAA'
+	});
 });
 
-// @route   POST api/comments
-// @desc    Add new comment
-// @access  Private
-router.post(
-  '/',
-  [
-    auth,
-    [
-      check('content', 'Content is required')
-        .not()
-        .isEmpty()
-    ]
-  ],
-  async (req, res) => {
-    // Validate input
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      // If there are errors, return 400 (bad request)
-      return res.status(400).json({ errors: errors.array() });
-    }
+app.get('/users', function(req, res){
+	res.render('users/index', {
+		users: db.get('users').value()
+	});
+});
 
-    // Extract data from request
-    const { content } = req.body;
+app.get('/users/search', function(req, res){
+	var q = req.query.q;
+	var matchedUsers = db.get('users').value().filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/index', {
+		users: matchedUsers,
+		q: q
+	});
+});
 
-    try {
-      // Create new comment
-      const newComment = new Comment({
-        content,
-        user: req.user.id
-      });
+app.get('/users/create', function(req, res){
+	res.render('users/create');
+});
 
-      // Save to database
-      const comment = await newComment.save();
+app.get('/users/:id', function(req, res){
+	var id = req.params.id;
+	var user = db.get('users').find({ id: id }).value();
+	res.render('users/view', {
+		user: user
+	});
+});
 
-      // Return comment
-      res.json(comment);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send('Server error (POST api/comments)');
-    }
-  }
-);
+app.post('/users/create', function(req, res){
+	req.body.id = shortid.generate();
+	db.get('users').push(req.body).write();
+	res.redirect('/users');
+});
 
-// @route   PUT api/comments/:id
-// @desc    Update comment
-// @access  Private
-router.put('/:id', auth, async (req, res) => {
-  // Extract data from request
-  const { content } = req.body;
-
-  // Build comment object
-  const commentFields = {};
-  if (content) commentFields.content = content;
-
-  try {
-    // Find comment by id
-    let comment = await Comment.findById(req.params.id);
-
-    // Check if comment exists
-    if (!comment) return res.status(404).json({ msg: 'Comment not found' });
-
-    // Check if user is owner of comment
+// 6. Listen port
+app.listen(3000, function(){
+	console.log('Server listening on port ' + 3000);
+});
